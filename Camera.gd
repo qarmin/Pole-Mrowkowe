77c8d6e05b20fb6956c01084d9d287f82e88f2e4
qarmin <mikrutrafal54@gmail.com>
2020-06-18 10:57:55 +0200

    Dodanie podstawowego przybliżania i oddalania

--- Camera.gd
+++ Camera.gd
@@ -2,51 +2,61 @@ extends Camera
 
 const LOOKAROUND_SPEED : float = 0.0025
 const MOVE_CAMERA : float = 0.0025
+const MOVE_SPEED : float = 1.0
 const MOVING_BOX : PoolVector3Array = PoolVector3Array([Vector3(0.0,0.0,0.0), Vector3(20.0,20.0,20.0)])# Wymiary paczki ograniczające gracza
-# accumulators
+
+# Używane w obracaniu obrazem
 onready var rot_x : float = 0.0
 onready var rot_y : float = 0.0
 
-var camera_key_changed : bool = false 
+var projection_changed : bool = false 
+
+enum CAMERA_MOVEMENT {BACK_SCROLL,FORWARD_SCROLL,LEFT,RIGHT,UP,DOWN}
+
+
 
 func _ready() -> void:
 	# Ustawia zmienne na pozycje ustawione w edytorze(nic nie zmienia)
 	rot_x = get_rotation().y
 	rot_y = get_rotation().x
 
-func _input(event):
+func _input(event) -> void:
 	# Klawiatura
 	if event is InputEventKey:
 		if event.is_pressed():
-			if event.get_scancode() == KEY_DOWN:
-				if !camera_key_changed:
+			if event.get_scancode() == KEY_R: # Reset Kamery
+				#
+				pass
+			if event.get_scancode() == KEY_DOWN: # Zmiana projekcji
+				if !projection_changed:
 					if get_projection() == Camera.PROJECTION_PERSPECTIVE:
 						set_projection(Camera.PROJECTION_ORTHOGONAL)
 					else:
 						set_projection(Camera.PROJECTION_PERSPECTIVE)
-					camera_key_changed = true
+					projection_changed = true
+			if event.get_scancode():
+				pass
 		else:
 			if event.get_scancode() == KEY_DOWN:
-				camera_key_changed = false
+				projection_changed = false
 	
 	# Mysz - kliknięcie
 	if event is InputEventMouseButton:
 		if event.is_pressed():
 			if event.get_button_mask() == BUTTON_MASK_MIDDLE: # Środokowy przycisk myszy - Klik
-				print("ŚR")
 				pass
-			if event.get_button_mask() == 8:
-				print("8")
+			if event.get_button_mask() & 8: # Scroll w górę
+				move_camera(CAMERA_MOVEMENT.FORWARD_SCROLL)
 				pass
-			if event.get_button_mask() == 16:
-				print("16")
+			if event.get_button_mask() & 16: # Scroll w dół
+				move_camera(CAMERA_MOVEMENT.BACK_SCROLL)
 				pass
 	
 	# Mysz - poruszanie
 	if event is InputEventMouseMotion:
 		if event.get_button_mask() == BUTTON_LEFT: # Lewy przycisk myszy
 			pass
-		if event.get_button_mask() == BUTTON_RIGHT: # Prawy przycisk myszy
+		if event.get_button_mask() == BUTTON_RIGHT: # Prawy przycisk myszy - na chwilę obraca kamerą wokół własnej osi Y
 			rot_x -= event.relative.x * LOOKAROUND_SPEED
 			rot_y -= event.relative.y * LOOKAROUND_SPEED
 			
@@ -56,6 +66,31 @@ func _input(event):
 			transform.basis = Basis()
 			rotate_object_local(Vector3(0, 1, 0), rot_x) # first rotate in Y
 			rotate_object_local(Vector3(1, 0, 0), rot_y) # then rotate in X
+
+
+
+func move_camera(roman : int) -> void:
+	var move_vec : Vector3 = Vector3()
+	match(roman):
+		CAMERA_MOVEMENT.BACK_SCROLL:
+			move_vec.z += 1
+		CAMERA_MOVEMENT.FORWARD_SCROLL:
+			move_vec.z -= 1
+		CAMERA_MOVEMENT.UP:
+			move_vec.z -= 1
+		CAMERA_MOVEMENT.DOWN:
+			move_vec.z += 1
+		CAMERA_MOVEMENT.LEFT:
+			move_vec.x -= 1
+		CAMERA_MOVEMENT.RIGHT:
+			move_vec.x += 1
+		_:
+			printerr("Nie nadano poprawnej wartości dla move_camera")
+			pass
+	#move_vec = move_vec.rotated(Vector3(0,1,0),rotation_degrees.y)
+	translate(move_vec * MOVE_SPEED)
+	# TODO # Clamp pozycje gracza aby nie wychodził poza obszar
+	#global_translate(move_vec * MOVE_SPEED)
 #
 #func _ready() -> void:
 #	pass
--- Performance.gd
+++ Performance.gd
@@ -1,10 +1,10 @@
 extends VBoxContainer
 
-func _ready():
-	for i in range(18):
+func _ready() -> void:
+	for _i in range(18):
 		add_child(load("res://Menu/Performance/SinglePerf.tscn").instance())
 
-func _process(delta):
+func _process(_delta) -> void:
 	get_child(0).set_text("FPS: " + str(Performance.get_monitor(Performance.TIME_FPS)))
 	get_child(1).set_text("TIME_PROCESS: " + str(Performance.get_monitor(Performance.TIME_PROCESS)))
 	get_child(2).set_text("TIME_PHYSICS_PROCESS: " + str(Performance.get_monitor(Performance.TIME_PHYSICS_PROCESS)))
@@ -20,6 +20,6 @@ func _process(delta):
 	get_child(12).set_text("RENDER_DRAW_CALLS_IN_FRAME: " + str(Performance.get_monitor(Performance.RENDER_DRAW_CALLS_IN_FRAME)))
 	get_child(13).set_text("RENDER_2D_ITEMS_IN_FRAME: " + str(Performance.get_monitor(Performance.RENDER_2D_ITEMS_IN_FRAME)))
 	get_child(14).set_text("RENDER_2D_DRAW_CALLS_IN_FRAME: " + str(Performance.get_monitor(Performance.RENDER_2D_DRAW_CALLS_IN_FRAME)))
-	get_child(15).set_text("RENDER_VIDEO_MEM_USED: " + str(Performance.get_monitor(Performance.RENDER_VIDEO_MEM_USED)))
-	get_child(16).set_text("RENDER_TEXTURE_MEM_USED: " + str(Performance.get_monitor(Performance.RENDER_TEXTURE_MEM_USED) / 1000000.0) + "MB")
-	get_child(17).set_text("RENDER_VERTEX_MEM_USED: " + str(Performance.get_monitor(Performance.RENDER_VERTEX_MEM_USED)))
+	get_child(15).set_text("RENDER_VIDEO_MEM_USED: " + str(stepify(Performance.get_monitor(Performance.RENDER_VIDEO_MEM_USED) / 1000000.0,0.01)) + "MB")
+	get_child(16).set_text("RENDER_TEXTURE_MEM_USED: " + str(stepify(Performance.get_monitor(Performance.RENDER_TEXTURE_MEM_USED) / 1000000.0,0.01)) + "MB")
+	get_child(17).set_text("RENDER_VERTEX_MEM_USED: " + str(stepify(Performance.get_monitor(Performance.RENDER_VERTEX_MEM_USED) / 1000000.0,0.01)) + "MB")
