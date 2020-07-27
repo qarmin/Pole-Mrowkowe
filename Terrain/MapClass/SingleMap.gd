a3ac41e6b46123d80fda20b5d377622e1e088fb2
qarmin <mikrutrafal54@gmail.com>
2020-07-27 22:01:19 +0200

    Usunięcie offsetu bo powoduje więcej problemów niż korzyści

--- Terrain/MapClass/SingleMap.gd
+++ Terrain/MapClass/SingleMap.gd
@@ -65,75 +65,56 @@ func calculate_number_of_terrains():
 
 
 func shrink_map() -> void:
-	var real_min_x: int = -1
 	var real_max_x: int = -1
-	var real_min_y: int = -1
 	var real_max_y: int = -1
 
-	print_map(fields)
+#	print_map(fields)
 	for y in size.y:
 		for x in size.x:
 			if fields[y][x] != MapCreator.FIELD_TYPE.NO_FIELD:
-				if real_min_x == -1:
-					real_min_x = x
+				if real_max_x == -1:
 					real_max_x = x
 				else:
-					if real_min_x > x:
-						real_min_x = x
 					if real_max_x < x:
 						real_max_x = x
-				if real_min_y == -1:
-					real_min_y = y
+				if real_max_y == -1:
 					real_max_y = y
 				else:
-					if real_min_y > y:
-						real_min_y = y
 					if real_max_y < y:
 						real_max_y = y
-
-	if real_min_y % 2 == 1:
-		starts_with_offset = true
-	else:
-		starts_with_offset = false
 	
-	if (real_max_x - real_min_x + 1) != fields[0].size() || (real_max_y - real_min_y + 1) != fields.size():
-		print("TODO: Trzeba zmniejszyć mapę")
+	if (real_max_x + 1) != fields[0].size() || (real_max_y + 1) != fields.size():
+#		print("INFO: Trzeba przyciąć mapę")
 		var new_fields: Array = []
-		for y in range(real_min_y, real_max_y + 1):
+		for y in range(real_max_y + 1):
 			new_fields.append([])
-			for x in range(real_min_x, real_max_x + 1):
-				new_fields[y - real_min_y].append(fields[y][x])
+			for x in range(real_max_x + 1):
+				new_fields[y].append(fields[y][x])
 
-		var base_offset: int = (real_min_y * int(size.x)) + real_min_x  # Baza tego ile należy usunąć numerów
-		var second_offset: int = (real_min_x) + (int(size.x) - real_max_x - 1) # Ilość numerów do usunięcia wraz z każdym nowym poziomem
-		# Zmiana nazw hexów
-		print("--- PRZED")
-		for i in map.get_children():
-			print(i.get_name())
-		print_map(fields)
+#		print("--- PRZED")
+#		for i in map.get_children():
+#			print(i.get_name())
+#		print_map(fields)
 
 		for i in map.get_children():
 			var number: int = i.get_name().trim_prefix(MapCreator.NODE_BASE_NAME).to_int()
 # warning-ignore:integer_division
-			var old_y: int = number / int(size.x) # Numerowanie od 0
-# warning-ignore:integer_division
-			var new_y: int = number / int(size.x) - real_min_y
-# warning-ignore:integer_division
-			var old_x: int = number % int(size.x)
-			#var new_y : int = number / # Numerowanie od 0
-			number -= base_offset + second_offset * new_y
+			var old_y: int = number / int(size.x) 
+			#var old_x: int = number % int(size.x)
+			
+			number -= (int(size.x) - real_max_x - 1) * old_y
 			i.set_name(MapCreator.NODE_BASE_NAME + str(number))
 
 		fields = new_fields
-		print("--- PO")
-		for i in map.get_children():
-			print(i.get_name())
-		print_map(fields, starts_with_offset)
+#		print("--- PO")
+#		for i in map.get_children():
+#			print(i.get_name())
+#		print_map(fields)
 
 		set_size(Vector2(new_fields[0].size(), new_fields.size()), false)
 
-	else:
-		print("INFO: Nie trzeba zmniejszać mapy")
+#	else:
+#		print("INFO: Nie trzeba zmniejszać mapy")
 
 
 func initialize_fields() -> void:
@@ -151,11 +132,11 @@ func initialize_full_fields() -> void:
 	number_of_terrain = number_of_all_possible_hexes
 
 
-static func print_map(array: Array, using_offset: bool = false) -> void:
+static func print_map(array: Array) -> void:
 	print("Printed map")
 	for i in range(array.size()):
 		var line: String = ""
-		if i % 2 == int(!using_offset):
+		if i % 2 == 1:
 			line += "  "
 		for j in range(array[i].size()):
 			line += str(array[i][j]) + "  "
