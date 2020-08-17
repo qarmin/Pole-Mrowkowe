99eb05d5bf18d878748f31ef05d02d9d8bdc1e57
Rafał Mikrut <mikrutrafal54@gmail.com>
2020-08-17 20:08:25 +0200

    Przesunięcie systemu zapisu do Autoload

--- Autoloads/MapCreator.gd
+++ Autoloads/MapCreator.gd
@@ -312,42 +312,6 @@ func populate_map_buildings(_single_map: SingleMap) -> void:
 	push_warning("TODO - Tworzenie głównej siedziby i być może jakichś podstawowych budynków, może być przydatne przy tworzeniu mapy")
 
 
-## Zapisuje mapę do pliku jako PackedScene
-## Dostępne jest to tylko do celów testowych
-func save_map_as_packed_scene(single_map: SingleMap, destroy: bool = false) -> void:
-	var packed_scene = PackedScene.new()
-	packed_scene.pack(single_map.map)
-
-	if ResourceSaver.save("res://GeneratedMap.tscn", packed_scene) != OK:
-		printerr("Nie powiodła się próba zapisu mapy")
-
-	if destroy == true:
-		single_map.map.queue_free()
-
-
-#	var packed_scene = PackedScene.new()
-#	packed_scene.pack(map)
-#	if(ResourceSaver.save("user://GeneratedMap.tscn", packed_scene) != OK):
-
-#	if(ResourceSaver.save("res://GeneratedMap.tscn", packed_scene) != OK):
-#		printerr("Nie powiodła się próba zapisu mapy")
-#
-#	map.queue_free()
-
-
-func save_map_as_text(single_map: SingleMap) -> void:
-	var file_to_save: File = File.new()
-	if file_to_save.open("saved_slot1.txt", File.WRITE) != OK:
-		push_error("Nie udało się utworzyć pliku do zapisu")
-
-	file_to_save.store_8(1)  # Wersja savu, może być przydatna przy wczytywaniu
-
-	file_to_save.store_var(single_map.size)
-	file_to_save.store_var(single_map.fields)
-	file_to_save.store_var(single_map.units)
-
-	file_to_save.close()
-	pass
 
 
 ## Tworzy mapę, którą można użyć w Scene Tree(Jest to zwykły Node)
--- Autoloads/SaveSystem.gd
+++ Autoloads/SaveSystem.gd
@@ -6,17 +6,39 @@ var file_handler: File = File.new()
 var create_file_backup: bool = false  #Tworzy kopie savefile.sav savefile.backup.sav savefile.backup2.sav
 
 
-func _ready() -> void:
-	pass
+## Zapisuje mapę do pliku jako PackedScene
+## Dostępne jest to tylko do celów testowych
+func save_map_as_packed_scene(single_map: SingleMap, destroy: bool = false) -> void:
+	var packed_scene = PackedScene.new()
+	packed_scene.pack(single_map.map)
+
+	if ResourceSaver.save("res://GeneratedMap.tscn", packed_scene) != OK:
+		printerr("Nie powiodła się próba zapisu mapy")
+
+	if destroy == true:
+		single_map.map.queue_free()
 
 
-func save_game() -> void:
-	if create_file_backup:
-		pass
-	else:
-		pass
-	pass
+#	var packed_scene = PackedScene.new()
+#	packed_scene.pack(map)
+#	if(ResourceSaver.save("user://GeneratedMap.tscn", packed_scene) != OK):
+
+#	if(ResourceSaver.save("res://GeneratedMap.tscn", packed_scene) != OK):
+#		printerr("Nie powiodła się próba zapisu mapy")
+#
+#	map.queue_free()
 
 
-func load_game() -> void:
+func save_map_as_text(single_map: SingleMap) -> void:
+	var file_to_save: File = File.new()
+	if file_to_save.open("saved_slot1.txt", File.WRITE) != OK:
+		push_error("Nie udało się utworzyć pliku do zapisu")
+
+	file_to_save.store_8(1)  # Wersja savu, może być przydatna przy wczytywaniu
+
+	file_to_save.store_var(single_map.size)
+	file_to_save.store_var(single_map.fields)
+	file_to_save.store_var(single_map.units)
+
+	file_to_save.close()
 	pass
