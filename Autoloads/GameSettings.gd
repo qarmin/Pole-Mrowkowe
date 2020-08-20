cecfe046d6af39490b81cdc4bc81f2951cc3ce60
Rafał Mikrut <mikrutrafal54@gmail.com>
2020-08-20 19:47:42 +0200

    Dodanie w miarę ładnej i prostej wody

--- Autoloads/GameSettings.gd
+++ Autoloads/GameSettings.gd
@@ -3,6 +3,10 @@ extends Node
 var game_time: float = 0
 const MAX_TEAMS: int = 4  # Służy do sprawdzania czy wszędzie zaaktualizowano tę liczbę gdzie tylko możliwe 
 enum GAME_TYPE { SKIRMISH, CAMPAIGN}
+var game_type : int # game type
+var single_map : SingleMap = null
+var game_started: bool = false
 
 func _process(delta: float) -> void:
-	game_time += delta
+	if game_started == true:
+		game_time += delta
