9d181783ca8c9d31c9054c4790983758cbf4fe25
Rafał Mikrut <mikrutrafal@protonmail.com>
2021-05-16 13:57:48 +0200

    Aktualizacja map w benchmarku i menu

--- Tests/MapCreatorTests.gd
+++ Tests/MapCreatorTests.gd
@@ -10,10 +10,10 @@ func _ready() -> void:
 func test_create_map():
 	var single_map: SingleMap = SingleMap.new()
 
-	MapCreator.create_map(single_map, Vector2j.new(5, 5), 5)
+	MapCreator.create_map(single_map, Vector2j.new(10, 10), 4)
 #	MapCreator.populate_map_realistically(single_map, 4)
-#	MapCreator.populate_map_randomly(single_map, 50)
-	MapCreator.populate_map_randomly_playable(single_map, 50)
+	MapCreator.populate_map_randomly(single_map, 50)
+#	MapCreator.populate_map_randomly_playable(single_map, 50)
 	MapCreator.create_3d_map(single_map)
 #	PreviewGenerator.generate_preview_image(single_map)
 	add_child(single_map.map)
